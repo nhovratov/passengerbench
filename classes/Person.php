3e31bbd387b57b589f5b8d800d81c2db2e85894d
Nikita Hovratov <nikita.h@live.de>
2018-02-20 09:49:18 +0100

    Fix person model

--- classes/Person.php
+++ classes/Person.php
@@ -1,104 +1,130 @@
 <?php
 
-class Person{
-	/*
-	* @var string
-	*/
-	private $firstname;
-	/*
-	* @var string
-	*/
-	private $lastname;
-	/*
-	* @var string
-	*/
-	private $email; 
-	/*
-	* @var string
-	*/
-	private $licensePlate;
-	/*
-	* @var string
-	*/
-	private $picture;
-	/*
-	* @var string
-	*/
-	private $password;
-	
-	/*
-	* @return string
-	*/
-	public function getFirstname (){
-		return $this->firstname;
-	}
-	/*
-	* @param string
-	*/
-	public function setFirstname (string $firstname){
-		$this->firstname = $firstname;
-	}
-	
-	/*
-	* @return string
-	*/
-	public function getLastname (){
-		return $this->lastname;
-	}
-	/*
-	* @param string
-	*/
-	public function setLastname (string $lastname){
-		$this->lastname = $lastname;
-	}
-	/*
-	* @return string
-	*/
-	public function getEmail (){
-		return $this->email;
-	}
-	/*  
-	* @param string
-	*/
-	public function setEmail (string $email){
-		$this->email = $email;
-	}
-	/*
-	* @return string
-	*/
-	public function getLicensePlate (){
-		return $this->licensePlate;
-	}
-	/*  
-	* @param string
-	*/
-	public function setLicensePlate (string $licensePlate){
-		$this->licensePlate = $licensePlate;
-	}
-	/*
-	* @return string
-	*/
-	public function getPicture (){
-		return $this->picture;
-	}
-	/*  
-	* @param string
-	*/
-	public function setPicture ((string) $picture){
-		$this->picture = $picture;
-	}
-	/*
-	* @return string
-	*/
-	public function getPassword (){
-		return $this->password;
-	}
-	/*  
-	* @param string
-	*/
-	public function setPassword ((string) $password){
-		$this->password = $password;
-	}
-}
+class Person
+{
+    /**
+     * @var string
+     */
+    private $firstname = "";
 
-?>
+    /**
+     * @var string
+     */
+    private $lastname = "";
+
+    /**
+     * @var string
+     */
+    private $email = "";
+
+    /**
+     * @var string
+     */
+    private $licensePlate = "";
+
+    /**
+     * @var string
+     */
+    private $picture = "";
+
+    /**
+     * @var string
+     */
+    private $password = "";
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param mixed $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLicensePlate()
+    {
+        return $this->licensePlate;
+    }
+
+    /**
+     * @param mixed $licensePlate
+     */
+    public function setLicensePlate($licensePlate)
+    {
+        $this->licensePlate = $licensePlate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+
+    /**
+     * @param mixed $picture
+     */
+    public function setPicture($picture)
+    {
+        $this->picture = $picture;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
