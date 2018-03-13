33db9717f2a31b3048a3f46599f8d897a0ab9785
Dogi78 <e.dogs@web.de>
2018-03-13 10:32:36 +0100

    data protection declaration

---
+++ datenschutz.php
@@ -0,0 +1,63 @@
+<h1>Erklärung zum Datenschutz</h1>
+<br>
+<p>
+     Sie erhalten als Nutzer unserer Internetseite in dieser Datenschutzerklärung alle notwendigen Informationen darüber, 
+     wie, in welchem Umfang sowie zu welchem Zweck wir oder Drittanbieter Daten von Ihnen erheben und diese verwenden. 
+     Die Erhebung und Nutzung Ihrer Daten erfolgt streng nach den Vorgaben des Bundesdatenschutzgesetzes (BDSG) und des Telemediengesetzes (TMG). 
+     Wir fühlen uns der Vertraulichkeit Ihrer personenbezogenen Daten besonders verpflichtet und arbeiten deshalb streng innerhalb der Grenzen, 
+     die die gesetzlichen Vorgaben uns setzen. Die Erhebung dieser personenbezogenen Daten erfolgt auf freiwilliger Basis, wenn uns das möglich ist. 
+     Auch geben wir diese Daten nur mit Ihrer ausdrücklichen Zustimmung an Dritte weiter. Wir sorgen bei besonders vertraulichen Daten wie im 
+     Zahlungsverkehr oder im Hinblick auf Ihre Anfragen an uns durch Einsatz einer SSL-Verschlüsselung für hohe Sicherheit. Wir möchten es aber 
+     an dieser Stelle nicht versäumen, auf die allgemeinen Gefahren der Internetnutzung hinzuweisen, auf die wir keinen Einfluss haben. Besonders 
+     im E-Mail-Verkehr sind Ihre Daten ohne weitere Vorkehrungen nicht sicher und können unter Umständen von Dritten erfasst werden.
+</p>
+<h2>Auskunft, Löschung, Sperrung</h2>
+<p>
+     Sie erhalten jederzeit unentgeltlich Auskunft über die von uns gespeicherten personenbezogenen Daten zu Ihrer Person sowie zur Herkunft, 
+     dem Empfänger und dem Zweck von Datenerhebung sowie Datenverarbeitung. Außerdem haben Sie das Recht, die Berichtigung, die Sperrung oder 
+     Löschung Ihrer Daten zu verlangen. Ausgenommen davon sind Daten, die aufgrund gesetzlicher Vorschriften aufbewahrt oder zur ordnungsgemäßen 
+     Geschäftsabwicklung benötigt werden. Damit eine Datensperre jederzeit realisiert werden kann, werden Daten zu Kontrollzwecken in einer Sperrdatei 
+     vorgehalten. Werden Daten nicht von einer gesetzlichen Archivierungspflicht erfasst, löschen wir Ihre Daten auf Ihren Wunsch. Greift die 
+     Archivierungspflicht, sperren wir Ihre Daten. Für alle Fragen und Anliegen zur Berichtigung, Sperrung oder Löschung von personenbezogenen 
+     Daten wenden Sie sich bitte an unseren Datenschutzbeauftragten unter den Kontaktdaten in dieser Datenschutzerklärung bzw. an die im Impressum genannte Adresse.
+</p>
+<h2>Erhebung von Zugriffsdaten</h2>
+<p>
+     Die Auslieferung und Darstellung der Inhalte über unsere Webseite erfordert technisch die Erfassung bestimmter Daten. 
+     Mit Ihrem Zugriff auf unsere Webseite werden diese sogenannten Server-Logfiles durch uns oder den Provider des Webspace erfasst. 
+     Diese Logfiles erlauben keinen Rückschluss auf Sie und Ihre Person.
+
+     Die entsprechenden Informationen bestehen aus dem Namen der Webseite, der Datei, dem aktuellen Datum, der Datenmenge, 
+     dem Webrowser und seiner Version, dem eingesetzten Betriebssystem, dem Domain-Namen Ihres Internet-Providers, der Referrer-URL als jene Seite, 
+     von der Sie auf unsere Seite gewechselt sind, sowie der entsprechenden IP-Adresse.
+
+     Wir nutzen diese Daten zur Darstellung und Auslieferung unserer Inhalte sowie zu statistischen Zwecken. 
+     Die Informationen unterstützen die Bereitstellung und ständige Verbesserung unseres Angebots. 
+     Auch behalten wir uns vor, die erwähnten Daten nachträglich zu prüfen, sollte der Verdacht auf eine rechtswidrige Nutzung unseres Angebotes bestehen.
+</p>
+<h2>Inhalte und Dienste von Drittanbietern</h2>
+<p>
+     Das Angebot auf unserer Webseite erfasst unter Umständen auch Inhalte, Dienste und Leistungen von anderen Anbietern, die unser Angebot ergänzen. 
+     Beispiele für solche Angebote sind Karten von Google-Maps, YouTube-Videos oder Grafikdarstellungen Dritter. Der Aufruf dieser Leistungen von dritter 
+     Seite erfordert regelmäßig die Übermittlung Ihrer IP-Adresse. Damit ist es diesen Anbietern möglich, Ihre Nutzer-IP-Adresse wahrzunehmen und diese 
+     auch zu speichern.
+     Wir bemühen uns sehr, nur solche Drittanbieter einzubeziehen, die IP-Adressen allein zur Auslieferung der Inhalte nutzen. Wir haben dabei jedoch keinen 
+     Einfluss darauf, welcher Drittanbieter gegebenenfalls die IP-Adresse speichert. Diese Speicherung kann zum Beispiel statistischen Zwecken dienen. 
+     Sollten wir von Speicherungsvorgängen durch Drittanbieter Kenntnis erlangen, weisen wir unsere Nutzer unverzüglich auf diese Tatsache hin.
+     Beachten Sie bitte in diesem Zusammenhang auch die speziellen Datenschutzerklärungen zu einzelnen Drittanbietern und Dienstleistern, 
+     deren Service wir auf unserer Webseite nutzen. Sie finden Sie ebenfalls in dieser Datenschutzerklärung.
+</p>
+<h2>Personenbezogene Daten</h2>
+<p>
+     Personenbezogene Daten erheben wir im Rahmen von Datenvermeidung und Datensparsamkeit nur in dem Ausmaß und so lange, wie es zur Nutzung unserer Webseite notwendig ist, beziehungsweise vom Gesetzgeber vorgeschrieben wird. Wir nehmen den Schutz Ihrer persönlichen Daten ernst und halten uns bei Erhebung und Verarbeitung personenbezogener Daten streng an die entsprechenden gesetzlichen Vorschriften und an diese Datenschutzerklärung. Fällt der Zweck der Datenerhebung weg oder ist das Ende der gesetzlichen Speicherfrist erreicht, werden die erhobenen Daten gesperrt oder gelöscht.
+     Regelmäßig kann unsere Webseite ohne die Weitergabe persönlicher Daten genutzt werden. Wenn wir personenbezogene Daten erheben – etwa Ihren Namen, Ihre Anschrift oder Ihre E-Mail-Adresse – erfolgt diese Datenerhebung freiwillig. Ohne eine ausdrücklich erteilte Zustimmung von Ihrer Seite werden diese Daten Dritten nicht zur Kenntnis gebracht.
+     Beachten Sie bitte, dass Daten im Internet allgemein nicht immer sicher übertragen werden. Besonders im E-Mail-Verkehr kann der Schutz beim Datenaustausch nicht garantiert werden.
+</p>
+<h2>SSL-Verschlüsselung</h2>
+<p>
+     Unsere Webseite bedient sich einer SSL-Verschlüsselung, wenn es um die Übermittlung vertraulicher oder persönlicher Inhalte unserer Nutzer geht. 
+     Diese Verschlüsslung wird zum Beispiel bei der Abwicklung des Zahlungsverkehrs sowie bei Anfragen aktiviert, die Sie an uns über unsere Webseite stellen. 
+     Achten Sie bitte darauf, dass die SSL-Verschlüsselung bei entsprechenden Aktivitäten von Ihrer Seite her aktiviert ist. Der Einsatz der Verschlüsselung 
+     ist leicht zu erkennen: Die Anzeige in Ihrer Browserzeile wechselt von „http://“ zu „https://“. Über SSL verschlüsselte Daten sind nicht von Dritten lesbar. 
+     Übermitteln Sie Ihre vertraulichen Informationen nur bei aktivierter SSL-Verschlüsselung und wenden Sie sich im Zweifel an uns.
+</p>
